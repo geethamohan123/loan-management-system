97133231264320522c07b8e19b3ee45ae404e170
Geethamohan <geethamohan.kollu@nephroplus.com>
2025-07-27 15:13:14 +0530

    Moduler service based project structure created .

---
+++ common-lib/src/test/java/com/loan_mgmt/common_lib/CommonLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_mgmt.common_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/test/java/com/loan_mgmt/payment_service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan_mgmt.payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
